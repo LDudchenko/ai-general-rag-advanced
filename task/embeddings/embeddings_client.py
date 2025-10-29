a7bd154aff34eeba43abd137d5a0d2b5a6e93333
Solmariana <dudchenkoliudmyla@gmail.com>
2025-10-29 21:12:13 +0200

    Implemented embedding client

--- task/embeddings/embeddings_client.py
+++ task/embeddings/embeddings_client.py
@@ -1,52 +1,35 @@
-import json
-
 import requests
 
-#TODO:
-# ---
-# https://platform.openai.com/docs/api-reference/embeddings
-# ---
-# Implement EmbeddingsClient:
-# - constructor should apply model name and api key
-# - endpoint is https://api.openai.com/v1/embeddings
-# - create method `get_embeddings` that will generate embeddings for input list (don't forget about dimensions)
-#   with Embedding model and return back a dict with indexed embeddings (key is index from input list and value vector list)
 
 class EmbeddingsClient:
     _endpoint: str
     _api_key: str
+    _model_name: str
 
     def __init__(self, model_name: str, api_key: str):
         if not api_key or api_key.strip() == "":
             raise ValueError("API key cannot be null or empty")
 
-        self._endpoint = 'https://api.openai.com/v1/embeddings'
+        self._endpoint = "https://api.openai.com/v1/embeddings"
         self._api_key = "Bearer " + api_key
         self._model_name = model_name
 
-# Hint:
-# Request:
-# curl https://api.openai.com/v1/embeddings \
-#   -H "Content-Type: application/json" \
-#   -H "Authorization: Bearer $OPENAI_API_KEY" \
-#   -d '{
-#     "input": "Your text string goes here",
-#     "model": "text-embedding-3-small",
-#     "dimensions": 384
-#   }'
-#
-#  Response JSON:
-#  {
-#     "data": [
-#         {
-#             "embedding": [
-#                 0.19686688482761383,
-#                 ...
-#             ],
-#             "index": 0,
-#             "object": "embedding"
-#         }
-#     ],
-#     ...
-#  }
+    def get_embeddings(self, texts: list[str], dimensions: int = 384) -> dict[int, list[float]]:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self._api_key
+        }
 
+        payload = {
+            "input": texts,
+            "model": self._model_name,
+            "dimensions": dimensions
+        }
+
+        response = requests.post(self._endpoint, headers=headers, json=payload)
+        if response.status_code != 200:
+            raise Exception(f"Request failed: {response.status_code}, {response.text}")
+
+        data = response.json()
+        result = {item["index"]: item["embedding"] for item in data["data"]}
+        return result
